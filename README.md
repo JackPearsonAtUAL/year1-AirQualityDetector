80c190f6f5b1d021d6fa093b4863108e30f5977c
JackPearsonAtUAL <j.pearson0220251@arts.ac.uk>
2026-05-15 17:27:42 +0100

    Added code for the dust sensor. Updated ReadMe. Added images for evidence.

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
-# year1-AirQualityDetector
+# Year 1: Air Quality Detector
+## Components
+    Arduino Wifi R4
+
+    Mikroe Air Quality Click
+
+    Grove Dust Sensor PPD42
+
+    Breadboard
+
+    LCD Screen Nokia 5110
+
+## Language
+    C++
+## Libraries / Packages
+    MQ135_XCR
+
+## Images / Video
+    year1-AirQualityDetector/supplementaries
+
+## Design Process
+
+## Hardware Process
+1. Integrating the Air Quality Click
+2. Integrating the Dust Sensor
+3. Integrating the Nokia Screen
+
+## Programming Process
+1. Initial sensor: Air Quality Click
+2. Initial output: LED
+3. Second sensor: Dust Sensor
+4. Change of output: LED to text on screen
+
+## Personal thoughts
 
--- year1-AirQualityDetector.ino
+++ year1-AirQualityDetector.ino
@@ -1,4 +1,7 @@
 /*
+Dev note:
+This must be run in the arduino IDE, not from. VSCode
+
 Required Libraries:
 MQ135_XCR.h
 
@@ -10,6 +13,15 @@ const int PIN_RED   = 4;
 const int PIN_GREEN = 3;
 const int PIN_BLUE  = 2;
 
+// Dust Sensor
+int dustPin = 13;
+unsigned long duration;
+unsigned long starttime;
+unsigned long sampletime_ms = 10000;//sampe 30s ;
+unsigned long lowpulseoccupancy = 0;
+float ratio = 0;
+float concentration = 0;
+
 // Air click board (Detects toxic gases; ammonia (NH3), nitrogen oxides (NOx) benzene, smoke, CO2, +more)
 // Detects between 10 - 1000
 // Map the value to a percent by dividing it by 10
@@ -18,30 +30,31 @@ MQ135_XCR airSensor(A0);
 
 void setup() {
   Serial.begin(9600);
+
   airSensor.calibrate();
+
+  // Setup pins
+  pinMode(dustPin, INPUT);
   pinMode(PIN_RED,   OUTPUT);
   pinMode(PIN_GREEN, OUTPUT);
   pinMode(PIN_BLUE,  OUTPUT);
-  
+
+  // Get the current time
+  starttime = millis();
+
+  Serial.print("This program will output the air quality every ");
+  Serial.print(String(sampletime_ms/1000));
+  Serial.print(" seconds");
+  Serial.println('\n');
 }
 
 void loop() {
-  airCheck();
-  colorChange(52, 168);
+  if ((millis()-starttime) > sampletime_ms){
+    airCheck();
+  }
 
-  delay(2000);
 }
 
-void checkColour(String s){
-  if (s == "")
-}
-
-// B is always 0
-void colorChange(int R, int G){
-  analogWrite(PIN_RED,   R);
-  analogWrite(PIN_GREEN, G);
-  analogWrite(PIN_BLUE,  0);
-}
 
 void airCheck(){
   gasCheck();
@@ -57,8 +70,24 @@ void gasCheck(){
     Serial.print(ppm/10);
     Serial.print("% | Air Quality: ");
     Serial.println(airQuality); 
+    Serial.println('\n');
 }
 
 void particleCheck(){
-  return;
+  duration = pulseIn(dustPin, LOW);
+    lowpulseoccupancy = lowpulseoccupancy+duration;
+
+    ratio = lowpulseoccupancy/(sampletime_ms*10.0);  // Integer percentage 0=>100
+    concentration = 1.1*pow(ratio,3)-3.8*pow(ratio,2)+520*ratio+0.62; // using spec sheet curve
+    
+    Serial.print("Low Pulse Occupancy:");
+    Serial.print(lowpulseoccupancy);
+    Serial.print(", Ratio:");
+    Serial.print(ratio);
+    Serial.print(", Dust Concentration:");
+    Serial.println(concentration);
+    Serial.println('\n');
+
+    lowpulseoccupancy = 0;
+    starttime = millis();
 }
